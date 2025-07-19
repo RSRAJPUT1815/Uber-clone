d3771a4f21ec82dc1b4e54d99753035c8ee13397
RSRAJPUT1815 <rsrajput1815@gmail.com>
2025-07-19 14:41:46 +0530

    Day12 ui desine

---
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+const LocationSearchPanel = () => {
+    const locations = [
+        {
+            title: 'Kempegowda International Airport...',
+            address: 'KIAL Rd, Devanahalli, Bengaluru, Karnataka',
+        },
+        {
+            title: 'Phoenix Marketcity',
+            address: 'Whitefield Rd, Devasandra Industrial Estate, Mahadevapura, Bengaluru, Karnataka',
+        },
+        {
+            title: 'Salarpuria Aura Block B',
+            address: 'BLOCK-B, TOUCH STONE, Chandana, Kadabesanahalli, Bengaluru, Karnataka',
+        },
+        {
+            title: 'Sheraton Grand Bengaluru Whitefield...',
+            address: 'Prestige Shantiniketan Hoodi, Whitefield, Thigalarapalya, Krishnarajapuram, Bengaluru',
+        },
+        {
+            title: 'KSR Bengaluru City Junction (Bangalore)',
+            address: 'M.G. Railway Colony, Majestic, Bengaluru, Karnataka',
+        },
+    ];
+    return (
+        <div className="w-full h-screen bg-white p-4 space-y-4">
+            {/* Header */}
+            <div className="flex justify-between items-center">
+                <button className="border border-gray-400 px-4 py-2 rounded-md text-sm text-gray-700">
+                    Leave Now ▼
+                </button>
+            </div>
+
+            {/* Location List */}
+            <div className="space-y-4">
+
+                {locations.map((loc, index) => (
+                    <div key={index} className="flex gap-4 items-start mt-4">
+                        <h5 className="text-2xl bg-[#eee] p-3 rounded-full">
+                            <i className="ri-map-pin-line"></i>
+                        </h5>
+                        <div>
+                            <h4 className="text-md font-semibold text-black leading-tight">{loc.title}</h4>
+                            <p className="text-sm text-gray-600">{loc.address}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            {/* Set location link */}
+            <div className="pt-4 border-t border-gray-300">
+                <button className="flex items-center gap-2 text-blue-600 font-semibold text-sm">
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default LocationSearchPanel
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,22 +1,48 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap';
+import 'remixicon/fonts/remixicon.css'
+import LocationSearchPanel from '../components/LocationSearchPanel';
 
 const Home = () => {
+  const [pickup, setPickup] = useState('');
+  const [destination, setDestination] = useState('');
+  const [panelOpen, setPanelOpen] = useState(false);
+  const panalRef = useRef(null);
+
+  useGSAP(function () {
+    if (panelOpen) {
+      gsap.to(panalRef.current, {
+        height: '70%'
+
+      });
+    } else {
+      gsap.to(panalRef.current, {
+        height: '0%'
+      });
+    }
+  }, [panelOpen]);
+
   return (
     <div className='relative h-screen w-screen'>
       <img src="uber.png" alt="uber-logo" className='w-28 absolute left-3 top-4' />
       <div>
         <img src="uber-car-map.webp" alt="temp" className=' w-full ' />
       </div>
-      <div className='bg-white absolute top-10  h-screen p-5 rounded-t-3xl shadow-lg w-full'>
-        <div className='h-[30%]'>
+      <div className='  absolute top-0  h-screen  w-full flex flex-col justify-end'>
+        <div className='h-[30%] relative bg-white rounded-t-3xl pt-4 px-3'>
           <h4 className='text-3xl font-semibold'>Find a trip</h4>
-          <form className='flex flex-col gap-4 p-4 w-full'>
-            <input type="text" placeholder='Add a pick-up location' className='w-full border-none outline-none px-4 text-xl font-semibold py-4 rounded-xl bg-[#e4e3e3]' />
-            <input type="text" placeholder='Enter your destination' className='w-full border-none outline-none px-4 text-xl font-semibold py-4 rounded-xl bg-[#e4e3e3]' />
+          <form className='flex flex-col  gap-4 p-4 w-full' onSubmit={(e) => e.preventDefault()}>
+            <h5 className='absolute top-4 right-4 text-3xl' onClick={() => setPanelOpen(!panelOpen)}>
+              {panelOpen ? <i className="ri-arrow-down-s-line"></i> : <i className="ri-arrow-up-s-line"></i>}
+            </h5>
+            <div className='h-28 w-1 bg-gray-900 absolute top-20 left-10 rounded-full '></div>
+            <input type="text" onClick={() => setPanelOpen(true)} value={pickup} onChange={(e) => { setPickup(e.target.value) }} placeholder='Add a pick-up location' className='w-full border-none outline-none px-4 pl-7 text-xl font-semibold py-4 rounded-xl bg-[#e4e3e3]' />
+            <input type="text" value={destination} onClick={() => { setPanelOpen(true) }} onChange={(e) => { setDestination(e.target.value) }} placeholder='Enter your destination' className='w-full border-none outline-none px-4 pl-7 text-xl font-semibold py-4 rounded-xl bg-[#e4e3e3]' />
           </form>
         </div>
-        <div className='h-[70%] bg-red-500'>
-
+        <div ref={panalRef} className='h-[0%]  bg-white '>
+          <LocationSearchPanel/>
         </div>
       </div>
     </div>
